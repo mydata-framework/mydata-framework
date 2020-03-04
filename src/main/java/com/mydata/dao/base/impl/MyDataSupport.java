f1af285bf8cea1484d6e75e92df70bb4279baa16
liutao <yumeng@sdscm.com>
2020-03-04 19:14:38 +0800

    init project

--- src/main/java/com/mydata/dao/base/impl/MyDataSupport.java
+++ src/main/java/com/mydata/dao/base/impl/MyDataSupport.java
@@ -690,8 +690,7 @@ public abstract class MyDataSupport<POJO> implements IMyData<POJO> {
                         Entry<String, Object> en = ite.next();
                         for (PropInfo p : pps) {
                             if (p.getPname().equals(en.getKey())) {
-                                buf.append(p.getCname()).append(KSentences.EQ.getValue())
-                                        .append(KSentences.POSITION_PLACEHOLDER.getValue());
+                                buf.append("`").append(p.getCname()).append("`").append(KSentences.EQ.getValue()).append(KSentences.POSITION_PLACEHOLDER.getValue());
                                 if (ite.hasNext()) {
                                     buf.append(KSentences.COMMA.getValue());
                                 }
