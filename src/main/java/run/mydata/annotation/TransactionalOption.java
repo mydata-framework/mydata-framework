fb658250ccbaaa8916cc354620424c27b6911354
liutao <tao.liu@weimob.com>
2020-08-10 14:42:29 +0800

    init 8.0

--- src/main/java/run/mydata/annotation/TransactionalOption.java
+++ src/main/java/run/mydata/annotation/TransactionalOption.java
@@ -5,10 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 选择性的事务管理
+ *
+ * @author Liu Tao
+ */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface TransactionalOption {
 
+    /**
+     * 连接管理器名称
+     * @return .
+     */
     String[] connectionManagerNames() default {};
 
 }
